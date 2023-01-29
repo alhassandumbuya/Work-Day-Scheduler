9b733ec42719f8434b427b2c08b039982366e9a7
Alhassan Dumbuya <alhassandumbuya@Alhassans-MacBook-Air.local>
2023-01-29 21:21:40 +0000

    commit before moving the timeblocks to js

--- starter/script.js
+++ starter/script.js
@@ -1,2 +1,28 @@
 // Display the current day at the top of the calender when a user opens the planner.
 let presentDay = $('#currentDay').html(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+$(document).ready(function() {
+
+    $(".saveBtn").on("click", function () {
+        let scheduale = $('.col-md-10').val();
+        localStorage.setItem("plan", scheduale);
+        console.log(scheduale);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
