d40b4af4c63da684c84c79cb3473d25a7a0249c6
Alhassan Dumbuya <alhassandumbuya@Alhassans-MacBook-Air.local>
2023-01-30 20:53:33 +0000

    added for loop for each of the days

--- starter/script.js
+++ starter/script.js
@@ -9,14 +9,41 @@ $(document).ready(function() {
         console.log(scheduale);
 })
 
+let dayHours = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+let $timeBlockContainerEl = $(".container");
+for (let i = 0; i < dayHours.length; i++) {
+    console.log(dayHours);
+    $timeBlockContainerEl.append(
+        <div class="row time-block">
+        <div class="col-md-1 hour">
+
+        </div>
+        <textarea class="col-md-10 description future">
+        </textarea>
+        <button class="col-md-1 saveBtn i:hover">
+          Save
+        </button>
+      </div>
+    )
 
 
+    
+}
 
 
-
-
-
-
+// dayHours.forEach((hour, index) => {
+//     $timeBlockContainerEl.append(
+  
+//       <div class="row time-block">
+//           <div class="col-md-1 hour">
+//             8am
+//           </div>
+//           <textarea class="col-md-10 description future">
+//           </textarea>
+//           <button class="col-md-1 saveBtn i:hover">
+//             Save
+//           </button>
+//         </div>
 
 
 
