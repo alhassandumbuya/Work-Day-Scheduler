75d59f85eeff318e78d72aeaf7ef82d42d979249
Alhassan Dumbuya <alhassandumbuya@Alhassans-MacBook-Air.local>
2023-01-30 22:17:54 +0000

    figuring out the localstore

--- starter/script.js
+++ starter/script.js
@@ -3,22 +3,25 @@ let presentDay = $('#currentDay').html(moment().format('MMMM Do YYYY, h:mm:ss a'
 
 $(document).ready(function() {
 
-    $(".saveBtn").on("click", function () {
-        let scheduale = $('.col-md-10').val();
-        localStorage.setItem("plan", scheduale);
-        console.log(scheduale);
+    $(".saveBtn").on("click", function (event) {
+        console.log(event.target);
+        let taskDescription=$(event.target).siblings("textarea").val() //targeting the save button
+        localStorage.setItem(`${(event.target).attr("data-hour")}`,taskDescription) //should save the string(key/tome/hour) and the value(discription)
+        // let scheduale = $('.col-md-10').val();
+        // localStorage.setItem("plan", scheduale);
+        // console.log(scheduale);
 })
 
 let dayHours = [9, 10, 11, 12, 1, 2, 3, 4, 5];
 let $timeBlockContainerEl = $(".container");
 for (let i = 0; i < dayHours.length; i++) {
-    console.log(dayHours);
+    //console.log(dayHours);
     $timeBlockContainerEl.append(
        ` 
        <div class="row time-block">
         <div class="col-md-1 hour">${dayHours[i]}${i >= 3 ? "PM" : "AM"}
         </div>
-        <textarea class="col-md-10 description">
+        <textarea class="col-md-10 description" data-hour="${i+9}"> 
         </textarea>
         <button class="col-md-1 saveBtn">
           Save
@@ -28,7 +31,7 @@ for (let i = 0; i < dayHours.length; i++) {
      //  var text = $(this).siblings(".description").val();
      //  var time = $(this).parent().attr("id");
      )
-     localStorage.setItem(`${dayHours[i]}${i >= 3 ? "PM" : "AM"}`, $("textarea").val()) //saves the dayhours into the local storage
+    //  localStorage.setItem(`${dayHours[i]}${i >= 3 ? "PM" : "AM"}`, $("textarea").val()) //saves the dayhours into the local storage
      
     
   
